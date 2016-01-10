a5a2fc2273477a6fabdcd378dbfa2945da1da871
duzhongbo <710409599@qq.com>
2016-01-10 23:31:52 +0800

    渲染虚拟图片

--- index.js
+++ index.js
@@ -3,4 +3,12 @@ for(var i=1;i<=16;i++){
 	aData.push('./img/'+i+'.jpg');
 }
 
-
+//
+function create(aData){
+	var sHtml = '';
+	for(var i=0,len=aData.length;i<len;i++){
+		sHtml+='<li style="background-image:url('+aData[i]+')"></li>'
+	}
+	picList.innerHTML=sHtml;
+}
+create(aData);
